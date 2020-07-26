602d043f9fd472c529cc722001b5e3b03d68b377
zwt <1255284280@qq.com>
2020-07-26 21:09:58 +0800

    添加uview，设置首页tab标签栏

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,59 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view class="tabs">
+		<!-- 可拖动顶部选项卡 -->
+		<u-tabs ref="tabSwiper" @change="tabChange" :current="currentIndex" :list="list"
+		 :is-scroll="true" active-color="#df663a"></u-tabs>
+
+		<index-home v-if="currentIndex == 0"></index-home>
 	</view>
 </template>
 
 <script>
+	import indexHome from "./subPages/indexHome"
 	export default {
+		components: {
+			indexHome
+		},
+
 		data() {
 			return {
-				title: 'Hello'
+				list: [ {
+						name: '首页'
+					}, {
+						name: '手机'
+					}, {
+						name: '百货'
+					},
+					{
+						name: '女装'
+					},
+					{
+						name: '医药'
+					},
+					{
+						name: '数码'
+					},
+					{
+						name: '生活用品'
+					},
+					{
+						name: '箱包'
+					},
+				],
+				currentIndex: 0
 			}
 		},
-		onLoad() {
 
-		},
+		onLoad() {},
+
 		methods: {
-
+			tabChange( index ) {
+				this.currentIndex = index
+			}
 		}
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
---
+++ pages/index/subPages/indexHome.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>1231212</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
